af6024c6c5989aea2f2e68efe9420a46ee0cef87
alhamood <alberthamood@gmail.com>
2015-12-10 19:48:11 -0500

    Can create new users, passwords hashed

--- app.py
+++ app.py
@@ -2,72 +2,109 @@
 """
 Created on Tue Dec  8 14:02:36 2015
 
+Pyloric Database Web Server
+
 @author: alhamood
 """
 
-from flask import Flask, Response, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, g
 from flask.ext.login import LoginManager, UserMixin, login_required
-from itsdangerous import JSONWebSignatureSerializer
-from wtforms import Form, TextField, PasswordField, validators
+from wtforms import Form, validators, fields
 import os
 import sys
 import logging
-import flask.ext.login 
+import hashlib
+import flask.ext.login
+import simplejson as json
+
 
 app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 
-user_pdatabase = {'MarderUser': 'crabs',
-		'Admin': 'admin'}
+
+with open('user_pdatabase.txt') as json_data:
+	user_pdatabase = json.load(json_data)
+	json_data.close()
 		
-user_database = {'MarderUser': ('marder_email', 'marder_phone')}
-		
+
+with open('user_database.txt') as json_data:
+	user_database = json.load(json_data)
+	json_data.close()
+				
 		
 class User(UserMixin):	
 	pass
 
 
 class LoginForm(Form):
-	username = TextField('Username')
-	password = PasswordField('Password')
+	username = fields.TextField('Username')
+	password = fields.PasswordField('Password')
 
+
+class UserForm(Form):
+	username = fields.TextField('Username', [
+		validators.Length(max=20, message='Under 20 characters please!'),
+		validators.InputRequired(message='Username is required')
+		])
+	email = fields.TextField('email', [
+		validators.Email(message='Not an email address.')
+		])
+	surname = fields.TextField('Surname', [
+		validators.Length(max=25, message='Sorry 25 characters max'),
+		validators.InputRequired(message='Surname required')
+		])
+	lab = fields.TextField('Lab PI surname', [
+		validators.Length(max=20, message='20 characters max'),
+		validators.InputRequired(message='Lab PI surname required')
+		])
+	
+
+class NewUserForm(UserForm):
+	password = fields.PasswordField('Password', [
+		validators.InputRequired(message='Password Field(s) Empty'),
+		validators.EqualTo('confirm', message='Passwords unmatched.')
+	])
+	confirm = fields.PasswordField('Password')
+	
 
 @login_manager.user_loader
 def load_user(user_id):
-	print('hello from user_loader!')
 	if user_id not in user_pdatabase:
 		return		
 	user=User()
 	user.id = user_id
 	user.email = user_database[user_id][0]
-	user.phone = user_database[user_id][1]
+	user.surname = user_database[user_id][1]	
+	user.lab = user_database[user_id][2]
+	g.user = user
 	return user
-	
-
-@app.route('/login', methods=['post'])
-def login():
-	form = LoginForm(request.form)
-	if form.validate():
-		user=load_user(form.username.data)
-		if user == None:
-			return render_template('/unauthorized-page.html')		
-		if form.password.data == user_pdatabase[user.id]:
-			flask.ext.login.login_user(user)
-			return redirect(url_for('index'))		
-	return render_template('/unauthorized-page.html')
 
 
 @login_manager.unauthorized_handler
 def nope():
-	form = LoginForm()
 	return render_template('unauthorized-page.html')
 
 
-@app.route('/download-page', methods=['get', 'post'])
+@app.route('/login', methods=['POST'])
+def login():
+	form = LoginForm(request.form)
+	if form.validate():
+		user = load_user(form.username.data)
+		if user == None:
+			return render_template('/unauthorized-page.html')
+		if hashlib.sha256(form.data['password']).hexdigest() \
+				== user_pdatabase[user.id]:
+			flask.ext.login.login_user(user)
+			return redirect(url_for('index'))
+	return render_template('/unauthorized-page.html')
+
+
+@app.route('/download-page', methods=['GET', 'POST'])
 def download_page():
 	return render_template('download-page.html')
 
@@ -77,12 +114,45 @@ def download_page():
 def upload_page():
 	return render_template('upload-page.html')
 	
+	
+@app.route('/new-user', methods=['GET', 'POST'])
+def new_user():
+	if request.method == 'GET':
+		# get new user information, then come back as a post
+		form = NewUserForm()
+		return render_template('new-user.html', form=form)
+	else:
+		# create the user with validated user information
+		form = NewUserForm(request.form)
+		if form.validate():
+			user_database[form.data['username']] = \
+				(form.data['email'], form.data['surname'],
+				form.data['lab'])
+			user_pdatabase[form.data['username']] = \
+				(hashlib.sha256(form.data['password']).hexdigest())
+			with open('user_database.txt', 'w') as outfile:
+				json.dump(user_database, outfile)
+			with open('user_pdatabase.txt', 'w') as outfile:
+				json.dump(user_pdatabase, outfile)
+			return redirect(url_for('sign_in'))
+		else:
+			return render_template('new-user.html', form=form)
+
+
+@app.route('/admin-page')
+@login_required
+def admin_page():
+	if g.user.id == "Admin":
+		return render_template('admin-page.html')
+	else:
+		return redirect(url_for('index'))
+
 
 @app.route('/')
 def index():
 	return render_template('index.html')
 
-		
+
 @app.route('/sign-in')
 def sign_in():
 	form = LoginForm()
